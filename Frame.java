fd25794f7e8db7b6b653385d713a39f0738c2ce5
Chase Palmer <chase.palmer012704@gmail.com>
2023-02-16 09:31:11 -0500

    Created new frame for the application, and created Driver and Tester classes.  Driver will store the main game while Tester can be used for any testing

--- Frame.java
+++ Frame.java
@@ -1,1 +1,15 @@
+import javax.swing.JFrame;
+import java.awt.Color;
 
+public class Frame extends JFrame {
+
+    Frame() {
+    this.setTitle("Brick Breaker");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+    this.setSize(1000,720);
+    this.setResizable(false);
+    this.getContentPane().setBackground(new Color(000000));
+    }
+
+}
