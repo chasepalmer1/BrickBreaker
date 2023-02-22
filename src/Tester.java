e0423ada480fe0d3b6c1c1d933f82ab9c730972d
Steve Dillon <dillon22st@gmail.com>
2023-02-22 18:33:50 -0500

    Final fixing of the import and package statements.

--- src/Tester.java
+++ src/Tester.java
@@ -1,6 +1,14 @@
+import UI.*;
+import Inputs.*;
+
 public class Tester {
 
     public static void main(String[] args) {
-        new Frame();
+        Panel testingPanel = new Panel();
+        Frame testingFrame = new Frame(testingPanel);
+            
+        //According to the video I watched, this line is to direct your keyboard and mouse inputs to the Game Panel. Without it,
+        //your input won't do anything on the screen.
+        testingPanel.requestFocus();
     }
 }
