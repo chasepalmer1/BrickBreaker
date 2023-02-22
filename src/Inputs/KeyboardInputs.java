756f0d088ab5be2b46d8da43c87983704e8a1988
Steve Dillon <dillon22st@gmail.com>
2023-02-22 18:02:35 -0500

    Adding the Panel, Frame, Keyboard Inputs, and Mouse Inputs classes.

---
+++ src/Inputs/KeyboardInputs.java
@@ -0,0 +1,48 @@
+package src.Inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+
+public class KeyboardInputs implements KeyListener {
+	
+	private Panel gamePanel;
+	
+	public KeyboardInputs (Panel gamePanel) {
+		this.gamePanel = gamePanel;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_A:
+			gamePanel.keyInputSetXPos(-20);
+			break;
+		case KeyEvent.VK_D:
+			gamePanel.keyInputSetXPos(20);
+			break;
+		case KeyEvent.VK_RIGHT:
+			gamePanel.keyInputSetXPos(20);
+			break;
+		case KeyEvent.VK_LEFT:
+			gamePanel.keyInputSetXPos(-20);
+			break;
+		}
+			
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/Panel.java
@@ -0,0 +1,53 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyListener;
+import java.util.*;
+
+import javax.swing.JPanel;
+
+public class Panel extends JPanel {
+	
+	private int xPos = 100;
+	private int yPos = 630;
+	
+	
+	public Panel() {
+		addKeyListener(new KeyboardInputs(this));
+		addMouseListener(new MouseInputs(this));
+		addMouseMotionListener(new MouseInputs(this));
+	}
+	
+	public void keyInputSetXPos(int changeInX) {
+		if(xPos >= 0 && xPos <= 785) {
+			this.xPos += changeInX;
+		}
+		repaint();
+	}
+	
+	public void keyInputSetYPos(int changeInY) {
+		this.yPos += changeInY;
+		repaint();
+	}
+	
+	public void setXPosition(int xPos) {
+		if(xPos >= 0 && xPos <= 785) {
+			this.xPos = xPos;
+		}
+		repaint();
+	}
+	
+	public void setYPosition(int yPos) {
+		this.yPos = yPos;
+		repaint();
+	}
+	
+	public void paintComponent(Graphics g) {
+		//This "cleans" the surface, making painting our own objects smoother.
+		super.paintComponent(g);
+		
+		//xPos is the x coordinate on the panel, yPos is the y coordinate on the panel.
+		//200 is the width, and 50 is the height.
+		g.fillRect(xPos, yPos, 150, 30);
+	}
+
+}
